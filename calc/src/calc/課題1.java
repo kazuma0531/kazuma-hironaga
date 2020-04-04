3ef300b9c702ea97a123f7ac2c7ff31e39aae2f5
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-04 13:03:04 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/課題1.java
@@ -0,0 +1,17 @@
+//課題1
+package calc;
+
+public class 課題1 {
+
+	public static void main(String[] args) {
+		int a =(3 + 2 * 6 + 5), b = 15;
+		int c = 25, d = 3;
+
+		System.out.println("修正前: " + a);
+		System.out.println("修正後: " + (a + b));
+
+		System.out.println("25/3の商8 : 余り" + (c % d));
+
+	}
+
+}
---
+++ calc/src/calc/課題3.java
@@ -0,0 +1,23 @@
+//課題3
+package calc;
+
+import java.util.Scanner;
+
+public class 課題3 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner =  new Scanner (System.in);
+		int sum = 0;
+		for (int i = 0; i < 3; i++) {
+		System.out.print(i + 1 + "個目の数 = ");
+		sum += scanner.nextInt();
+		}
+		System.out.println("合計 = " + sum);
+		System.out.println("平均(3で割った時) = " + (int)sum / (int)3.0);
+		System.out.println("平均(3.0で割った時)= " + (double)sum /3.0);
+
+		scanner.close();
+	}
+
+}
