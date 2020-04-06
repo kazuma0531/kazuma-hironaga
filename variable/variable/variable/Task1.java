c5e57a0344d94e3f4259789ad9f5f67a2fae014e
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-06 18:25:08 +0900

    課題提出 - 変数・定数

---
+++ variable/variable/variable/Task1.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+	    double a = 31.2;
+	    String b = "Java研修";
+	    int c = 9;
+	    long d = 2147483648L;
+	    char f = '才';
+    }
+}
