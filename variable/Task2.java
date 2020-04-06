803d2ada3cb3ff370e5cd061d7097a05a9dd1207
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-06 18:30:58 +0900

    課題提出 - 変数・定数

---
+++ variable/Task2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		String name = "山田太朗";
+	    final  String TEL = "090-9999-1111";    //定数の宣言
+
+		System.out.println( name + "です。");     //山田太朗です。
+	    System.out.println("私の電話番号は\'" + TEL + "\'です。");      //私の電話番号は'090-9999-1111'です。
+		}
+}
