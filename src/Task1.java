2454114a790931f15d51076040625411eb6c0b40
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-06 17:56:36 +0900

    課題提出 - 四則演算

---
+++ src/Task1.java
@@ -0,0 +1,12 @@
+//課題1
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("修正前: " + (3 + 2 * 6 + 5));
+		System.out.println("修正後: " + ((3 + 2) * 6 + 5));
+        System.out.println("25/3の商"  + (25 / 3) +  " : " + "余り" + (25 % 3));
+
+	}
+
+}
