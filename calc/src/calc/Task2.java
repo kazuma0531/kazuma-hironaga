4d8a44e4f4559219f10b028dad46d81b68789124
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-07 19:35:25 +0900

    課題提出 - 四則演算

---
+++ calc/src/calc/Task2.java
@@ -0,0 +1,20 @@
+package calc;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 12, c = 13;
+		int d = (a + b + c);
+		int e = ((a + b + c) / 3);
+		double f = 3.0;
+		double g =  11.666666666666666;
+
+		System.out.println("合計: " + d); //合計: 35
+
+		System.out.println("平均(3で割ったとき): " + e); //平均(3で割ったとき): 11
+
+		System.out.println("平均(3.0で割ったとき): " + (d / f));    //平均(3.0で割ったとき):11.66666
+
+	}
+
+}
---
+++ calc/src/calc/Task3.java
@@ -0,0 +1,29 @@
+package calc;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+		
+	    
+		int a = scanner.nextInt();
+		    System.out.println(a);
+		int b = scanner.nextInt();
+		    System.out.println(b);
+		int c = scanner.nextInt();
+		    System.out.println(c);
+		int d = (a + b + c);
+		int e = ((a + b + c) / 3);
+		double f = ((a + b + c) / 3.0);
+		    
+		    System.out.println();
+		    System.out.println("合計: " + d);
+		    System.out.println("平均(3で割った時): " + e);
+		    System.out.println("平均(3.0で割った時): " + f);
+		    scanner.close();
+
+	}
+
+}
