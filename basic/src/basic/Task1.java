f99df35e128c44a87dbc59807820d835978ca759
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-08 15:14:03 +0900

    Javaの基礎　再コミット

---
+++ basic/src/basic/Task1.java
@@ -0,0 +1,12 @@
+//課題1
+package basic;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+	
+	System.out.println("初めての課題");    //初めての課題
+	System.out.println("Java攻略への道");  //Java攻略への道
+
+	}
+}
---
+++ basic/src/basic/Task2Java.java
@@ -0,0 +1,14 @@
+//課題2
+package basic;
+
+public class Task2Java {
+
+	public static void main(String[] args) {
+		/*/コメント
+		 * コメント
+		 */
+		System.out.println("hogehoge");
+	
+	System.out.println("hogehoge2");
+	}
+}
