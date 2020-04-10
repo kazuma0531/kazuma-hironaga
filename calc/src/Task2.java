592c3ba9b625cb9ef3ac42c6b58fda9347bd987c
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-10 15:41:17 +0900

    課題提出 - 四則演算

--- calc/src/Task2.java
+++ calc/src/Task2.java
@@ -6,15 +6,16 @@ public class Task2 {
 		int a = 10, b = 12, c = 13;
 		int d = (a + b + c);
 		int e = ((a + b + c) / 3);
-		double f = 3.0;
-		double g =  11.666666666666666;
+		double f = ((a + b + c) / 3.0);
+		
 
 		System.out.println("合計: " + d); //合計: 35
 
 		System.out.println("平均(3で割ったとき): " + e); //平均(3で割ったとき): 11
 
-		System.out.println("平均(3.0で割ったとき): " + (d / f));    //平均(3.0で割ったとき):11.66666
+		System.out.println("平均(3.0で割ったとき): " +  f);    //平均(3.0で割ったとき):11.66666
 
+		
 	}
 
 }
