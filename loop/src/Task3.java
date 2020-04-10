c2434143b2a648576ae86fdf2a0d7f3aec6b45cb
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-10 16:08:19 +0900

    課題提出 - ループ

---
+++ loop/src/Task3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("数字を入力してください: " );
+		int n = scanner.nextInt();
+		int a = 0;
+		while(n > 0){    //入力した回数が0になるまで処理
+			a ++;    //ブロック内の処理が終了した後、変数aを+1で実行される
+            n = n / 10;    //入力した整数を10で割る。
+	            
+		}
+		System.out.print("桁数: "  + a);
+		scanner.close();
+	}
+}
+/* 上記の想定結果
+ * 数字を入力してください: (数字を入力)
+ * 桁数: (10で割った回数分の数が出力)
+*/
---
+++ loop/src/Task4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("正の整数を入力してください: " );
+		
+		int number = scanner.nextInt();
+		int sum = 0;
+	    System.out.print(number + "の約数: ");
+	    for(int i = 1; i < number; i++) {	
+	    	if(number % i == 0) {
+	    		System.out.print(i + ",");
+       
+	    	}
+	    	sum = i + sum;
+	    	
+	    }
+	    System.out.print(number);
+	    scanner.close();
+	}
+}
+
