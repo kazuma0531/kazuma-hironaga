0195f21ea853f7c46ca04f6f325d3fd08971300c
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-17 17:15:46 +0900

    再提出 - ループ

--- loop/src/Task5.java
+++ loop/src/Task5.java
@@ -5,9 +5,9 @@ public class Task5 {
 		for(int i = 1; i <= 9; i++) {
 		    for(int j = 1; j <= 9; j++) {
 		        System.out.printf("%2d ", i * j);
-		        }
-		        System.out.println();
-		        }
+		    }
+		    System.out.println();
+		}
 	}
 
 }
