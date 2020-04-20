341236a2a5f6cf4e945de1db4361760ec91a4981
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-20 18:01:49 +0900

    再提出 - 配列

--- array/src/Task1.java
+++ array/src/Task1.java
@@ -2,7 +2,7 @@
 public class Task1 {
 
 	public static void main(String[] args) {
-        int[] array = {4,9,19,5,8,21,42,64,73,18,2};
+		int[] array = {4,9,19,5,8,21,42,64,73,18,2};
 		System.out.print("配列: [");
 		for(int i = 0; i < array.length; i++) {    //iはインデックス(0～10)を共有
 			if(i ==array.length -1) {    //lengthは配列変数の個数
