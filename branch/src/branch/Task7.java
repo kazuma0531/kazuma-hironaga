b2d35cac41b36154bed7999a7f65b2139228a16f
kazuma-hironaga <kazumahironaga@gmail.com>
2020-04-24 21:29:51 +0900

    再提出 - 条件分岐

--- branch/src/branch/Task7.java
+++ branch/src/branch/Task7.java
@@ -19,19 +19,19 @@ public class Task7 {
 			case "木曜":
 			case "金曜":
 				System.out.println("午前治療と午後治療があります");	//月曜から金曜の入力の場合
-			break;
+				break;
 			
 			case "土曜":
 				System.out.println("午前治療があります");			  //土曜の入力場合の場合
-			break;
+				break;
 			
 			case "日曜":
 				System.out.println("日曜は休診です");				  //日曜の入力の場合
-			break;
+				break;
 			
 			default:
 				System.out.println("入力を確認してください");		  //指定された文字列以外の入力の場合
-			break;
+				break;
 			
 		}
 		scanner.close();
